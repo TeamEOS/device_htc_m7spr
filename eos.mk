7f47f365b3343440194217f03e82c9b873464e12
Randall Rushing <randall.rushing@gmail.com>
2014-11-28 23:46:34 -0800

    Adapt for eos

--- eos.mk
+++ eos.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/eos/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
 
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:4.4.2/KOT49H/337390.3:user/release-keys" \
     PRIVATE_BUILD_DESC="5.03.651.3 CL337390 release-keys"
 
-PRODUCT_NAME := cm_m7spr
+PRODUCT_NAME := eos_m7spr
 PRODUCT_DEVICE := m7spr
